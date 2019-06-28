9781fd4a79786cf2596d7be759680d4e0641dfd6
fabio santos <fs.sep@hotmail.com>
2019-06-28 11:30:14 -0300

    inclusão do cors

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const cors = require('cors');
 const configdb = require('./config/app');
 
 const app = express();
@@ -12,6 +13,9 @@ mongoose.connect(configdb.database_url, {
   useNewUrlParser: true,
   useCreateIndex: true,
 });
+
+/* Liberar cors para qualquer requisição */
+app.use(cors());
 
 /* Utilizar json nas requisições */
 app.use(express.json());
